e9b74768f96dfe3c52f815db9a7dc93c357d5bac
Alex Vershinin <alexander.vershinin@kandasoft.com>
2021-02-09 19:50:20 +0300

    Initial commit

---
+++ test/test.spec.js
@@ -0,0 +1,11 @@
+const { expect } = require('chai');
+
+describe('suite', function () {
+    it('grep test1', function () {
+        expect(1).equals(1);
+    });
+
+    it('grep test2', function () {
+        expect(2).equals(2);
+    });
+});
